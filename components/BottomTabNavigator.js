b039e9378dfd507c9c39bf90e813ea379203dfe9
ARYAREPO2000 <117100919+ARYAREPO2000@users.noreply.github.com>
2023-08-08 10:38:12 +0400

    Add files via upload

---
+++ components/BottomTabNavigator.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import RideScreen from "../screens/Ride";
+import RideHistoryScreen from "../screens/RideHistory";
+
+const Tab = createBottomTabNavigator();
+
+export default class BottomTabNavigator extends React.Component {
+  render() {
+    return (
+      <NavigationContainer>
+        <Tab.Navigator>
+          {/*
+            <TabScreen name="Ride" component={RideScreen} />
+            <TabScreen name="Ride History" component={RideHistoryScreen} />
+          */}
+
+          {/* 
+            <Tab.Screen name="Ride" />
+            <Tab.Screen name="Ride History" />
+          */}
+
+          {/* 
+            <Screen name="Ride" component={RideScreen} />
+            <Screen name="Ride History" component={RideHistoryScreen} />
+          */}
+
+           
+            <Tab.Screen name="Ride" component={RideScreen} />
+            <Tab.Screen name="Ride History" component={RideHistoryScreen} />
+           
+        </Tab.Navigator>
+      </NavigationContainer>
+    );
+  }
+}
